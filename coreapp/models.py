59363a4a331f65eea4a923b7d37cb5da0662fe09
pratikaher <pratikaher88@gmail.com>
2019-01-04 09:16:29 +0530

    yes

--- coreapp/models.py
+++ coreapp/models.py
@@ -88,9 +88,9 @@ def create_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
 
 
-# @receiver(post_save, sender=User)
-# def save_profile(sender, instance, **kwargs):
-#     instance.profile.save()
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 # @receiver(post_save, sender=Book)
 # def create_user_list(sender, instance, created, **kwargs):
