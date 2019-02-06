b81ce9bc77e5fee187c23d81687a9e37ccfddd41
pratikaher <pratikaher88@gmail.com>
2019-02-06 23:53:18 +0530

    push

--- nofapapp/settings.py
+++ nofapapp/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     # 'dal',
     # 'dal_select2',
     'search',
-    'import_export',
+    # 'import_export',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
