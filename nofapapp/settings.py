5c4ce2715ef59735cac4459683e8fe13d30b7d1e
pratikaher <pratikaher88@gmail.com>
2019-02-07 09:00:03 +0530

    push

--- nofapapp/settings.py
+++ nofapapp/settings.py
@@ -169,10 +169,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
