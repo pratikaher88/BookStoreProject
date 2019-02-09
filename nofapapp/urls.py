58dbf31a1906bcf51650f1d89704a743cd2b8cb8
pratikaher <pratikaher88@gmail.com>
2019-02-09 12:09:36 +0530

    push

--- nofapapp/urls.py
+++ nofapapp/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
         name='password_reset_complete'),
 ] 
 
-if True:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                             document_root=settings.MEDIA_ROOT)
