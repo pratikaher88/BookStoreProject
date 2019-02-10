140069526bce555a5e2bb632b0d026fe3111bd29
pratikaher <pratikaher88@gmail.com>
2019-02-10 10:42:46 +0530

    models

--- coreapp/models.py
+++ coreapp/models.py
@@ -268,16 +268,16 @@ def create_profile(sender, instance, created, **kwargs):
         UserCollection.objects.create(owner=instance.profile)
 
 
-# class EmailThread(threading.Thread):
-#     def __init__(self, subject, html_content, recipient_list):
-#         self.subject = subject
-#         self.recipient_list = recipient_list
-#         self.html_content = html_content
-#         threading.Thread.__init__(self)
+class EmailThread(threading.Thread):
+    def __init__(self, subject, html_content, recipient_list):
+        self.subject = subject
+        self.recipient_list = recipient_list
+        self.html_content = html_content
+        threading.Thread.__init__(self)
 
-#     def run(self):
-#         msg = EmailMessage(self.subject, self.html_content, to=self.recipient_list)
-#         msg.send()
+    def run(self):
+        msg = EmailMessage(self.subject, self.html_content, to=self.recipient_list)
+        msg.send()
 
 
 @receiver(post_save, sender=User)
