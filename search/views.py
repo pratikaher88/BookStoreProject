84449e44395f57e1c65f0f192c67e524c91bb93b
pratikaher <pratikaher88@gmail.com>
2019-02-13 10:20:14 +0530

    push

--- search/views.py
+++ search/views.py
@@ -25,10 +25,6 @@ class SearchListView(LoginRequiredMixin, generic.ListView):
         requester_books = Transaction.objects.values_list('requester_book')
         offerrer_books = Transaction.objects.values_list('offerrer_book')
 
-        print(self.request.user)
-
-        print(Book.objects.exclude(user=self.request.user))
-
         return Book.objects.exclude(user=self.request.user).exclude(id__in=ordered_books).exclude(id__in=requester_books).exclude(id__in=offerrer_books).filter(Q(book_name__icontains=book_name) | Q(author_name__icontains=book_name))
 
         # return Book.objects.filter(Q(book_name__icontains=book_name)) | Book.objects.filter(Q(author_name__icontains=book_name))
